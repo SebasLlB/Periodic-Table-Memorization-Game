a300f88c6f9cd958d2c3bcf1875656c5f2af35e9
Sebastian Llauce <sebasllb01@gmail.com>
2026-08-05 17:17:13 -0500

    Began the periodicTable nested hashmap for each element. Each key holds another hashmap with the information on each element. With the aid of Deepseek and the pre-prompted alphabet I had it produce a list with each row which when printed together forms the symbol to be displayed in the element box.

--- mainFile.py
+++ mainFile.py
@@ -38,4 +38,20 @@
     to ask about and then popping their atomic number from the selection list.
     Finally, we check if the list is empty yet, and if not then we keep going until it's done.  
 '''
+#Let's start setting up an example hashmap for the Alkaline Metals
 
+periodicTable = {
+    "alkaline  metals":{
+        3  : {"name":"lithium", "atomicNumber":3, "symbol":["█       ","█     █ ","█       ","█    ███","█     █ ","█████ ███"]},
+        11 : {"name":"sodium", "atomicNumber":11, "symbol":["█   █     ","██  █     ","█ █ █  ███","█  ██ █   █","█   █ █████","█   █ █   █"]},
+        19 : {"name":"potassium", "atomicNumber":19, "symbol":[" █   █"," █  █ "," ███  "," █  █ "," █  █ "," █   █"]},
+        37 : {"name":"rubidium", "atomicNumber":37, "symbol":[" ███   █    "," █  █  █    "," ███   ███  "," █ █   █  █ "," █  █  █  █ "," █   █ ███  "]},
+        55 : {"name":"caesium", "atomicNumber":55, "symbol":["  ███         "," █   █  ███   "," █      █     "," █      ███   "," █   █     █  ","  ███   ███   "]},
+        87 : {"name":"francium", "atomicNumber":87, "symbol":[" █████      "," █          "," ███   ███  "," █     █    "," █     █    "," █     █    "]}
+    }
+}
+
+#now let me test smth
+for j in periodicTable["alkaline  metals"]:
+    for i in periodicTable["alkaline  metals"][j]["symbol"]:
+        print(i)
