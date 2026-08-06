126f4b0e7d28ce76c600f63d62ff534b39b1db45
Sebastian Llauce <sebasllb01@gmail.com>
2026-08-06 13:24:43 -0500

    Adding comments to explain certain boxes. I'll start with planning how I want each file to interact with each other, including what each file will be in charge of.

--- mainFile.py
+++ mainFile.py
@@ -3,15 +3,20 @@ from ptPrinting import printPeriodic
 from ptHashmap import periodicTable
 import random
 
+###This is where selected groups and their corresponding elements are stored
 chosenElements = []
 chosenGroups = ["alkaline metals","halogens","noble gases"]
 
+##Goes through every Periodic Table group name and see if it matches with the chosenGroups from the user\
+##If so, then retrieve all elements inside said groups
 for group in periodicTable:
     for chGroup in chosenGroups:
         if group == chGroup:
             for element in periodicTable[group]:
                 chosenElements.append(element)
 
+##This part goes through each element randomly and pulls out their symbol
+##Chosen elements list gets smaller as each element is retrieved, until there are no more elements left
 while len(chosenElements) > 0:
     print(chosenElements)
 
