9485f0c6d6bbdad6a5ffb85c826127c9c63d4c09
Sebastian Llauce <sebasllb01@gmail.com>
2026-08-05 21:26:07 -0500

    Adding additional files with big chunks of information: ptHashmap.py holds the periodic table hashmap, whilst ptPrinting.py holds the method to print the Periodic table and symbol replacement.

---
+++ ptHashmap.py
@@ -0,0 +1,29 @@
+### This is setting up the hashmap
+periodicTable = {
+    "alkaline metals":{
+        3  : {"name":"lithium", "atomicNumber":3, "symbol":["█       ","█     █ ","█       ","█    ███","█     █ ","█████ ███"]},
+        11 : {"name":"sodium", "atomicNumber":11, "symbol":["█   █     ","██  █     ","█ █ █  ███","█  ██ █   █","█   █ █████","█   █ █   █"]},
+        19 : {"name":"potassium", "atomicNumber":19, "symbol":[" █   █"," █  █ "," ███  "," █  █ "," █  █ "," █   █"]},
+        37 : {"name":"rubidium", "atomicNumber":37, "symbol":[" ███   █    "," █  █  █    "," ███   ███  "," █ █   █  █ "," █  █  █  █ "," █   █ ███  "]},
+        55 : {"name":"caesium", "atomicNumber":55, "symbol":["  ███         "," █   █  ███   "," █      █     "," █      ███   "," █   █     █  ","  ███   ███   "]},
+        87 : {"name":"francium", "atomicNumber":87, "symbol":[" █████      "," █          "," ███   ███  "," █     █    "," █     █    "," █     █    "]}
+    },
+    "alkaline earth metals":{
+
+    },
+    "post transition metals":{
+
+    },
+    "metalloids":{
+
+    },
+    "nonmetals":{
+
+    },
+    "halogens":{
+
+    },
+    "noble gases":{
+        
+    }
+}
